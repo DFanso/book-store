6de06e773de0d0eb8b4451a5f12b46bd22fd1d64
Leo Gavin <leogavin123@outlook.com>
2024-03-20 21:35:25 +0530

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { useRequestLogging } from './request-logging';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    rawBody: true,
+    bodyParser: true,
+  });
+  useRequestLogging(app);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('Book Store API')
+    .setDescription('Book Store API')
+    .setVersion('1.0')
+    .addTag('BookStore')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+
+  await app.listen(9000);
 }
 bootstrap();
